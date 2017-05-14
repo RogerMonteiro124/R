b6e2c5073be2104cea2956cdeadb715e5569c37f
Roger Monteiro <rogermonteiro15@outlook.com>
2017-05-14 17:37:56 -0400

    Update machines.R
    Adicionado mais duas maquinas e alterado configs do gráfico.

--- machines.R
+++ machines.R
@@ -4,14 +4,22 @@
 #Time:02:45:21
 
 #Calcular a probabilidade de uma maquina
-#produzir um produto com defeito
+#produzir um produto com defeito usando o Teorema de Bayes
+
+#############################################
+##              Teorema                    ##
+##   Pr(A|B)=(Pr(B|A) Pr(A))/Pr(B)         ##                                           
+##                                         ##
+#############################################
 main<-function(){
   #Maquinas
   #cada maquina possue 2 parametros: A quantidade produzida 
   #com defeito e a quantidade produzida no total
-  m1<-c(1,50)
-  m2<-c(2,40)
-  m3<-c(3,10)
+  m1<-c(1,35)
+  m2<-c(2,20)
+  m3<-c(3,15)
+  m4<-c(4,2)
+  m5<-c(1,18)
   
   #Essa funcao calcula a fraçao de vezes que a maquina é usada
   PMi<-function(Mi){
@@ -26,19 +34,25 @@ main<-function(){
     return(PMi(Mi)*PDMi(Mi))
   }
   
-  PD<-function(m1,m2,m3){
-      D=PDUMi(m1)+PDUMi(m2)+PDUMi(m3)
+  PD<-function(m1,m2,m3,m4,m5){
+      D=PDUMi(m1)+PDUMi(m2)+PDUMi(m3)+PDUMi(m4)+PDUMi(m5)
       return(D)
   }
-  D<-PD(m1,m2,m3)
+  D<-PD(m1,m2,m3,m4,m5)
   PM1D<-function(M1,D){
-    return((PDMi(M1)*PMi(M1))/D*100)
+    return(round((PDMi(M1)*PMi(M1))/D*100,1))
   }
   PM2D<-function(M2,D){
-    return((PDMi(M2)*PMi(M2))/D*100)
+    return(round((PDMi(M2)*PMi(M2))/D*100,1))
   }
   PM3D<-function(M3,D){
-    return((PDMi(M3)*PMi(M3))/D*100)
+    return(round((PDMi(M3)*PMi(M3))/D*100,1))
+  }
+  PM4D<-function(M4,D){
+    return(round((PDMi(M4)*PMi(M4))/D*100,1))
+  }
+  PM5D<-function(M5,D){
+    return(round((PDMi(M5)*PMi(M5))/D*100,1))
   }
   
   
@@ -57,12 +71,15 @@ main<-function(){
   
   #PLOTANDO O GRAFICO
   
-  grafico<-c(PM1D(m1,D),PM1D(m2,D),PM1D(m3,D))
-  pie(grafico,main = "Probabilidade de produzir um produto defeituoso.(%)",labels =c(PM1D(m1,D), PM1D(m2,D),PM1D(m3,D)),col = c(4,2,7))
+  grafico<-c(PM1D(m1,D),PM1D(m2,D),PM1D(m3,D),PM1D(m4,D),PM1D(m5,D))
+  graficolabels<-paste(grafico,"%",sep = "")
+  
+  pie(grafico,main = "Probabilidade de produzir um produto defeituoso.",labels = graficolabels,col = c(4,2,7,6,9))
   
   #Adcionando a Legenda
   
-  legend("topright",fill = c(4,2,7),legend = c("Maquina 1","Maquina 2","Maquina 3"))
+  legend("bottomright","(1,2)", cex=0.8,fill = c(4,2,7,6,9),legend = c("Maquina 1","Maquina 2","Maquina 3","Maquina 5","Maquina 5"))
 }
 main()
+
 
