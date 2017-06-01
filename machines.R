5113df942a508a60617ae5be2e00d0d9721eac51
Roger Monteiro <rogermonteiro15@outlook.com>
2017-06-01 02:19:10 -0400

    Update machines.R

--- machines.R
+++ machines.R
@@ -4,13 +4,8 @@
 #Time:02:45:21
 
 #Calcular a probabilidade de uma maquina
-#produzir um produto com defeito usando o Teorema de Bayes
+#produzir um produto com defeito
 
-#############################################
-##              Teorema                    ##
-##   Pr(A|B)=(Pr(B|A) Pr(A))/Pr(B)         ##                                           
-##                                         ##
-#############################################
 main<-function(){
   #Maquinas
   #cada maquina possue 2 parametros: A quantidade produzida 
@@ -40,16 +35,16 @@ main<-function(){
   }
   D<-PD(m1,m2,m3,m4,m5)
   PM1D<-function(M1,D){
-    return(round((PDMi(M1)*PMi(M1))/D*100,1))
+    return(round(((PDMi(M1)*PMi(M1))/D)*100,1))
   }
   PM2D<-function(M2,D){
-    return(round((PDMi(M2)*PMi(M2))/D*100,1))
+    return(round(((PDMi(M2)*PMi(M2))/D)*100,1))
   }
   PM3D<-function(M3,D){
-    return(round((PDMi(M3)*PMi(M3))/D*100,1))
+    return(round(((PDMi(M3)*PMi(M3))/D)*100,1))
   }
   PM4D<-function(M4,D){
-    return(round((PDMi(M4)*PMi(M4))/D*100,1))
+    return(round(((PDMi(M4)*PMi(M4))/D)*100,1))
   }
   PM5D<-function(M5,D){
     return(round((PDMi(M5)*PMi(M5))/D*100,1))
@@ -57,28 +52,39 @@ main<-function(){
   
   
   ###DEBUGS###
-  #PMi(m1)
-  #PMi(m2)
-  #PMi(m3)
-  #PDMi(m1)
-  #PDMi(m2)
-  #PDMi(m3)
-  #PDUMi(m1)
-  #PDUMi(m2)
-  #PDUMi(m3)
-  #PD(m1,m2,m3)
+  PMi(m1)
+  PMi(m2)
+  PMi(m3)
+  PMi(m4)
+  PMi(m5)
+  PDMi(m1)
+  PDMi(m2)
+  PDMi(m3)
+  PDMi(m4)
+  PDMi(m5)
+  PDUMi(m1)
+  PDUMi(m2)
+  PDUMi(m3)
+  PDUMi(m4)
+  PDUMi(m5)
+  PD(m1,m2,m3,m4,m5)
+  PM1D(m1,D)
+  PM2D(m2,D)
+  PM3D(m3,D)
+  PM4D(m4,D)
+  PM5D(m5,D)
   
   
   #PLOTANDO O GRAFICO
   
-  grafico<-c(PM1D(m1,D),PM1D(m2,D),PM1D(m3,D),PM1D(m4,D),PM1D(m5,D))
+  grafico<-c(PM1D(m1,D),PM2D(m2,D),PM3D(m3,D),PM4D(m4,D),PM5D(m5,D))
   graficolabels<-paste(grafico,"%",sep = "")
   
   pie(grafico,main = "Probabilidade de produzir um produto defeituoso.",labels = graficolabels,col = c(4,2,7,6,9))
   
   #Adcionando a Legenda
   
-  legend("bottomright","(1,2)", cex=0.8,fill = c(4,2,7,6,9),legend = c("Maquina 1","Maquina 2","Maquina 3","Maquina 5","Maquina 5"))
+  legend("bottomright","(1,2)", cex=0.8,fill = c(4,2,7,6,9),legend = c("Maquina 1","Maquina 2","Maquina 3","Maquina 4","Maquina 5"))
 }
 main()
 
